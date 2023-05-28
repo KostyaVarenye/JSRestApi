b02d26a80db176743195e23b252da6de5e371774
Kostya.Varennya <vekostya@gmail.com>
2023-05-28 11:56:05 +0300

    added middleware

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname)));
 
+// Middleware function
+const middleware = (req, res, next) => {
+  console.log('Hello from middleware');
+  next(); // Call next() to proceed to the next middleware or route handler
+};
+
+app.use(middleware); // Adding the middleware function here
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
